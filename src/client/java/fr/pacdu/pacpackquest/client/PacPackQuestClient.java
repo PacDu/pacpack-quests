5aa43e6478454ded081254107587cc8e58624099
PacDu <flagada30du30@gmail.com>
2026-08-30 00:09:28 +0200

    Initial commit

---
+++ src/client/java/fr/pacdu/pacpackquest/client/PacPackQuestClient.java
@@ -0,0 +1,10 @@
+package fr.pacdu.pacpackquest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PacPackQuestClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
